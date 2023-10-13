9672b15bdc9557c3bfef3d472fbfc91e1a35faf0
yanqi17118357 <3224848830@qq.com>
2023-10-13 11:39:31 +0800

    chore: add TreeNode definition

---
+++ lcof-java/src/definition/TreeNode.java
@@ -0,0 +1,14 @@
+package definition;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
