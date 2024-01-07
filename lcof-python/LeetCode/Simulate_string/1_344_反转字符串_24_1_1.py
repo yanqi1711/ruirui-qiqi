2692081463e37cce8bc200d00a32d1aa65d57e71
k9193 <1004278878@qq.com>
2024-01-07 20:23:27 +0800

    1_2

---
+++ lcof-python/LeetCode/Simulate_string/1_344_反转字符串_24_1_1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        # i = 0
+        # j, length = len(s) - 1,len(s) - 1
+        # for i in range(length//2 +1):
+        #     s[i],s[j] = s[j],s[i]
+        #     i+=1
+        #     j-=1
+        return s.reverse()
