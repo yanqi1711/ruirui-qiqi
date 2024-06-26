b074dbeac3392fdfaeb76d21afe1fc6a02f52e79
k9193 <1004278878@qq.com>
2024-06-26 10:15:39 +0800

    5_17

--- lcof-python/LeetCode/TreeAndListNode/Tree/85_671_二叉树中第二小的结点_5_17.py
+++ lcof-python/LeetCode/TreeAndListNode/Tree/85_671_二叉树中第二小的结点_5_17.py
@@ -8,6 +8,23 @@ class TreeNode:
         self.right = right
 class Solution:
     def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        ans = float('inf')
+        # 根据题意 root是最小的结点 所以需要dfs遍历找到第二大的
+        def dfs(r):
+            nonlocal ans
+            if not r:
+                return
+            dfs(r.left)
+            dfs(r.right)
+            # 大于最小值root.val 才有资格更新 因为有相等的
+            if r.val > root.val:
+                ans = min(ans, r.val)
+                return
+
+        dfs(root)
+        return ans if ans != float('inf') else -1
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
         ans = []
         def dfs(root):
             if not root:
