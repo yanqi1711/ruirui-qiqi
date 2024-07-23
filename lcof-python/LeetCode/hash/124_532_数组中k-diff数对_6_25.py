00fcb677b5f798c6ac24c814a8eedf41b20ad3e5
k9193 <1004278878@qq.com>
2024-07-23 13:54:49 +0800

    6_25

---
+++ lcof-python/LeetCode/hash/124_532_数组中k-diff数对_6_25.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        visited, res = set(), set()
+        for num in nums:
+            if num - k in visited:
+                res.add(num - k)
+            if num + k in visited:
+                res.add(num)
+            visited.add(num)
+        return len(res) 
