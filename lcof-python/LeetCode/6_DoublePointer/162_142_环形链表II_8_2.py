9c4e96f68de3ab84b70df61afee074f6e481c2d1
k9193 <1004278878@qq.com>
2024-09-14 22:20:08 +0800

    24_9_14

---
+++ lcof-python/LeetCode/6_DoublePointer/162_142_环形链表II_8_2.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
---
+++ lcof-python/LeetCode/6_DoublePointer/163_143_重排链表_8_3.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverstList(self, head):
+        if not head:
+            return None
+        pre = None
+        cur = head
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
+
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return None
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        new_head = self.reverstList(slow.next)
+        slow.next = None
+        first, second = head, new_head
+        while second:
+            # 记录后面2个结点
+            t1 = first.next
+            t2 = second.next
+            # 重新连接
+            first.next = second
+            second.next = t1
+            # 重置状态
+            first = t1
+            second = t2
+        return head
