d1da906c80d75aa71dfcb2e2a0a3c379596fb307
k9193 <1004278878@qq.com>
2024-09-15 21:12:37 +0800

    9_15

--- lcof-python/LeetCode/6_DoublePointer/162_142_环形链表II_8_2.py
+++ lcof-python/LeetCode/6_DoublePointer/162_142_环形链表II_8_2.py
@@ -1,8 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
--- lcof-python/LeetCode/6_DoublePointer/163_143_重排链表_8_3.py
+++ lcof-python/LeetCode/6_DoublePointer/163_143_重排链表_8_3.py
@@ -1,8 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
 class Solution:
     def reverstList(self, head):
         if not head:
